765686f7284eb158769adcd55f02521c732b3e95
ibrahim ethem ispir <ethem6046@gmail.com>
2022-02-03 19:01:23 +0300

    session ve cookie paketleri indirildi

--- app.js
+++ app.js
@@ -1,32 +1,38 @@
-const fastify = require("fastify")({ logger : true })
-const dotenv = require("dotenv").config({path: "./src/config/.env"})
-const port = process.env.PORT || 3003
-const router = require("./src/router/authRouter")
-fastify.register(require('fastify-cors'))
+const fastify = require("fastify")({ logger: true });
+const dotenv = require("dotenv").config({ path: "./src/config/.env" });
+const port = process.env.PORT || 3003;
+const router = require("./src/router/authRouter");
+fastify.register(require("fastify-cors"));
+const fastifySession = require("fastify-session");
+const fastifyCookie = require("fastify-cookie");
 
-require("./src/config/database")
-fastify.get("/" , async (req,reply) => {
-    return {message : "Merhaba"}
-})
+require("./src/config/database");
+fastify.get("/", async (req, reply) => {
+  return { message: "Merhaba" };
+});
+
+// sessions
+fastify.register(fastifyCookie,{maxAge:60*60*1000});
+fastify.register(fastifySession, { secret: process.env.SESSION_SECRET_KEY });
 
 router.forEach((route, index) => {
-    fastify.route(route)
-})
-
+  fastify.route(route);
+});
 
 const start = async () => {
-    try {
-        await fastify.listen(port)
-            .then(() => {
-                console.log(`Server ${port} connection successful`);
-            })
-            .catch((err) => {
-                console.log("Server connection Error");
-            })
-    } catch (err) {
-        fastify.log.error(err)
-        process.exit(1)
-    }
-}
+  try {
+    await fastify
+      .listen(port)
+      .then(() => {
+        console.log(`Server ${port} connection successful`);
+      })
+      .catch((err) => {
+        console.log("Server connection Error");
+      });
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
 
-start()
+start();
